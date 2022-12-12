8061f3727a1ab3304182611b8f10371c3434ad7a
Souhayl Maronesy <smaronesy@Souhayls-MacBook-Air.local>
2022-12-12 00:29:27 -0800

    added/update my wallet ui

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -3,24 +3,26 @@ import axios from 'axios';
 
 function Create({ pubKey }) {
     const receiverAddress = useRef()
+    const transactionAmount = useRef()
 
     var request = {
         "sender": pubKey,
         "receiver": "",
-        "amount": "10",
+        "amount": "",
         "timestamp": Date.now(),
         "signature": ""
     }
-    
+    console.log("HELLO" + pubKey.replace(/(\r\n|\n|\r)/gm, ""))
     // must also include transaction signature in request
 
     function handleAddTransaction(e) {
-        if (receiverAddress.current.value === '') return
+        if (receiverAddress.current.value === '' || transactionAmount.current.value === '' ) return
         request.receiver = receiverAddress.current.value;
+        request.amount = transactionAmount.current.value
         axios.post('/add_transaction', request)
             .then(res => {
-                console.log(res);
-                console.log(res.data);
+                // console.log(res);
+                // console.log(res.data);
             })
         window.location.reload();
     }
@@ -34,7 +36,7 @@ function Create({ pubKey }) {
             <form>
                 <lable>FROM  </lable>
                 <br />
-                <input disabled type="text" id="fname" name="firstname" placeholder={pubKey.replace('-----BEGIN PUBLIC KEY-----','')}/>
+                <input disabled type="text" id="fname" name="firstname" placeholder={pubKey}/>
                 <br />
                 <lable>To  </lable>
                 <br />
@@ -42,7 +44,7 @@ function Create({ pubKey }) {
                 <br />
                 <lable>Amount  </lable>
                 <br />
-                <input ref={receiverAddress} type="text" placeholder="Amount" />
+                <input ref={transactionAmount} type="text" placeholder="Amount" />
             </form>
             <br />
             <button className='create-btn' onClick={handleAddTransaction}>Add Transaction</button>
